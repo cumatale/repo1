ebb71a45aa3ee7fa90c556b82e23eb93f8d10589
Alexandru <alexandru.cumatrenco@gmail.com>
2023-06-15 19:43:30 +0300

    primul commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world2!");
     }
 
 }
